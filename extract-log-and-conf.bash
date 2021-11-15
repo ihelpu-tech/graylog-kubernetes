f17376df345647adc8505379e74b8dca0efd1eb3
ben-ihelputech <50592898+ben-ihelputech@users.noreply.github.com>
2021-11-15 09:16:15 -0600

    update to graylog 4.2.1
    - Updated to Graylog version 4.2.1
    - Modified command to capture stout to graylog-server.log for easier debugging.
    - fixed run bash and extraction scripts to use master as default target pod.

--- extract-log-and-conf.bash
+++ extract-log-and-conf.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Extracting server.conf and graylog-server.log..."
-PODNAME=$(kubectl get pods -n graylog | grep graylog-deployment | awk '{print$1}')
+PODNAME=$(kubectl get pods -n graylog | awk '/graylog\-master/ {print $1}')
 kubectl exec -n graylog -it $PODNAME -- cat /usr/share/graylog/log/graylog-server.log>graylog-server.log
 echo "Extracted Log"
 kubectl exec -n graylog -it $PODNAME -- cat /etc/graylog/server/server.conf>server.conf
--- runshellinpod.bash
+++ runshellinpod.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Getting shell inside pod..."
-PODNAME=$(kubectl get pods -n graylog | grep graylog-deployment | awk '{print$1}')
+PODNAME=$(kubectl get pods -n graylog | awk '/graylog\-master/ {print $1}')
 kubectl exec -n graylog -it $PODNAME -- bash
